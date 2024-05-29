5885491967d4e4ad37eaf308efc610630bb543c5
jingyyuu <jl12464@nyu.edu>
2024-05-29 00:21:59 -0400

    hi

--- solutions.py
+++ solutions.py
@@ -11,8 +11,15 @@ def calculate_profit():
     You can assume the user will enter only numeric characters, e.g. "3000", not "$3,000.00"
     The output should match the format of the following examples: "Profit: $690.00" for sales of $3,000, or "Profit: $2,300.00" for sales of $10,000, etc.
     """
+    # Ask the user to enter the projected amount of total sales
+    sales = float (input("Enter the projected amount of total sales: "))
 
+    # Calculate the annual profit as 23 percent of total sales
+    profit = sales * 0.23
 
+    # Display the profit
+    print("The profit that will be made from that amount is: ", profit)
+    
 def calculate_quotient_and_remainder():
     """
     Complete this function so that it asks the user to input two integers.
@@ -22,7 +29,13 @@ def calculate_quotient_and_remainder():
       Enter number #2: 2
       2 goes into 5 a total of 2 times with a remainder of 1
     """
-
+    # Ask the user to enter two integers
+    number_1 = int(input("Enter number #1: "))
+    number_2 = int(input("Enter number #2: "))
+    quotient= number_1/number_2
+    remainder=number_1%number_2
+    print("Quotient is:",quotient)
+    print("Remainder is:",remainder)
 
 def calculate_miles_per_gallon():
     """
@@ -35,6 +48,12 @@ def calculate_miles_per_gallon():
       Gas used (gallons): 25
       Miles per gallon: 2.2
     """
+    #Ask the user to enter Miles driven and Gallons of Gas Used
+    Miles_driven = float(input("Enter Miles driven:"))
+    Gallons_of_Gas_Used = float(input("Enter Gallons of Gas Used:"))
+    MPG = Miles_driven / Gallons_of_Gas_Used
+    print("Miles per gallon:",MPG)
+    
 
 
 def align_text():
@@ -53,3 +72,13 @@ def align_text():
       Price #2: $   10.00
       Price #3: $ 9532.60
     """
+    #Ask the user to enter in 3 price values (as floating point numbers)
+    price1 = float(input("Enter price #1: "))
+    price2 = float(input("Enter price #2: "))
+    price3 = float(input("Enter price #3: "))
+    
+    print("\nHere are your prices!\n")
+    
+    print(f"Price #1: ${price1:>10.2f}")
+    print(f"Price #2: ${price2:>10.2f}")
+    print(f"Price #3: ${price3:>10.2f}")
